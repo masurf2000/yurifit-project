63b06fec483c8d4b648a3dcb6b320d800d6807f2
Masayasu Saito <saitou@wales.jp>
2020-12-04 18:14:53 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteTitle: 'YURIFit Projects', // <title>
+  description: 'ジュニア選手育成計画', // meta description
+  keywords: '北海道,札幌,KBIKESERVICE,小橋,勇利', // meta keyword
+  manifestName: 'SolidState',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'YURIFit Projects - ジュニア選手育成計画',
+  subHeading: '目指せ、インターハイや全日本選手権で優勝できる全国レベルの北海道選手を排出！',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:anubhav.srivastava00@gmail.com',
+    },
+  ],
+  phone: '000-00000',
+  address: '305 Del Sol, Sunnyvale, CA',
+};
---
+++ src/pages/index.js
@@ -0,0 +1,163 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+import pic1 from '../assets/images/pic01.jpg';
+import pic2 from '../assets/images/pic02.jpg';
+import pic3 from '../assets/images/pic03.jpg';
+import pic4 from '../assets/images/pic04.jpg';
+import pic5 from '../assets/images/pic05.jpg';
+import pic6 from '../assets/images/pic06.jpg';
+import pic7 from '../assets/images/pic07.jpg';
+
+import config from '../../config';
+const IndexPage = () => (
+  <Layout>
+    <section id="banner">
+      <div className="inner">
+        <div className="logo">
+          <span className="icon fa-diamond"></span>
+        </div>
+        <h2>{config.heading}</h2>
+        <p>{config.subHeading}</p>
+      </div>
+    </section>
+
+    <section id="wrapper">
+      <section id="one" className="wrapper spotlight style1">
+        <div className="inner">
+          <a href="/#" className="image">
+            <img src={pic1} alt="" />
+          </a>
+          <div className="content">
+            <h2 className="major">プロジェクト概要</h2>
+            <p>
+              あああああああああああああああああああああああああああああああああああああ
+              いいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい
+              ううううううううううううううううううううううううううううううううううううう
+              えええええええええええええええええええええええええええええええええええええ
+              おおおおおおおおおおおお
+            </p>
+            <a href="/#" className="special">
+              Learn more
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section id="two" className="wrapper alt spotlight style2">
+        <div className="inner">
+          <a href="/#" className="image">
+            <img src={pic2} alt="" />
+          </a>
+          <div className="content">
+            <h2 className="major">選手・スタッフ紹介</h2>
+            <p>
+              あああああああああああああああああああああああああああああああああああああ
+              いいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい
+              ううううううううううううううううううううううううううううううううううううう
+              えええええええええええええええええええええええええええええええええええええ
+              おおおおおおおおおおおお
+            </p>
+            <a href="/#" className="special">
+              Learn more
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section id="three" className="wrapper spotlight style3">
+        <div className="inner">
+          <a href="/#" className="image">
+            <img src={pic3} alt="" />
+          </a>
+          <div className="content">
+            <h2 className="major">Sponsor</h2>
+            <p>
+              協賛企業様のご紹介ならびに、募集について
+            </p>
+            <a href="/#" className="special">
+              Learn more
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section id="four" className="wrapper alt style1">
+        <div className="inner">
+          <h2 className="major">活動記録・お知らせ</h2>
+          <p>
+            あああああああああああああああああああああああああああああああああああああ
+            いいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい
+            ううううううううううううううううううううううううううううううううううううう
+            えええええええええええええええええええええええええええええええええええええ
+            おおおおおおおおおおおお
+          </p>
+          <section className="features">
+            <article>
+              <a href="/#" className="image">
+                <img src={pic4} alt="" />
+              </a>
+              <h3 className="major">Sed feugiat lorem</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
+                nulla dignissim dapibus ultrices.
+              </p>
+              <a href="/#" className="special">
+                Learn more
+              </a>
+            </article>
+            <article>
+              <a href="/#" className="image">
+                <img src={pic5} alt="" />
+              </a>
+              <h3 className="major">Nisl placerat</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
+                nulla dignissim dapibus ultrices.
+              </p>
+              <a href="/#" className="special">
+                Learn more
+              </a>
+            </article>
+            <article>
+              <a href="/#" className="image">
+                <img src={pic6} alt="" />
+              </a>
+              <h3 className="major">Ante fermentum</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
+                nulla dignissim dapibus ultrices.
+              </p>
+              <a href="/#" className="special">
+                Learn more
+              </a>
+            </article>
+            <article>
+              <a href="/#" className="image">
+                <img src={pic7} alt="" />
+              </a>
+              <h3 className="major">Fusce consequat</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
+                nulla dignissim dapibus ultrices.
+              </p>
+              <a href="/#" className="special">
+                Learn more
+              </a>
+            </article>
+          </section>
+          <ul className="actions">
+            <li>
+              <a href="/#" className="button">
+                Browse All
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </section>
+  </Layout>
+);
+
+export default IndexPage;
